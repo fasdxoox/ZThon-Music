1be881717074fdb54a042c058b628160d3310b10
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-04-17 01:49:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 2000))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1895219306))
+OWNER_ID = int(getenv("OWNER_ID", 5901732027))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Sjshskdhd/ZThon-Music",
+    "https://github.com/fasdxoox/ZThon-Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
