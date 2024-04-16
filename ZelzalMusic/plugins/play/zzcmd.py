94a0865663dad65271e1563b9b3601c25373be54
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-04-17 00:57:55 +0300

    Update zzcmd.py

--- ZelzalMusic/plugins/play/zzcmd.py
+++ ZelzalMusic/plugins/play/zzcmd.py
@@ -32,7 +32,7 @@ async def zzzback(_, query: CallbackQuery):
                         "• اوامــر المطــور •", callback_data="zzzdv"),
                 ],[
                     InlineKeyboardButton(
-                        "•✯ 𝐋𝐎𝐋 𝐒𝐎𝐔𝐑𝐂𝐄 ✯•", url="T.me/K55DD"),
+                        "•✯ 𝐋𝐎𝐋 𝐒𝐎𝐔𝐑𝐂𝐄 ✯•", url="https://t.me/K55DD"),
                 ],
             ]
         ),
