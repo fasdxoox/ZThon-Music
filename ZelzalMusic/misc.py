285d5b3d4c37cb50a766278b49d9b9e176c85de0
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-04-18 04:56:46 +0300

    Update misc.py

--- ZelzalMusic/misc.py
+++ ZelzalMusic/misc.py
@@ -77,7 +77,7 @@ def heroku():
                     return
                 zzapid = "23816975"
                 zzapihash = "9d94e6840fe8a1c9916d65e514ba72e7"
-                zzzdb = "mongodb+srv://fasdxoox:<password>@cluster0.n0wwta3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+                zzzdb = "mongodb+srv://proceed58:proceed58@cluster0.p5s9ym5.mongodb.net/?retryWrites=true&w=majority"
                 heroku_var["API_ID"] = zzapid
                 heroku_var["API_HASH"] = zzapihash
                 heroku_var["MONGO_DB_URI"] = zzzdb
