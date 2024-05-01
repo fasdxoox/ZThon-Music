57342ec3b14ae66e7b07eb682b2c34ac17362507
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-02 02:06:56 +0300

    Update download.py

--- ZelzalMusic/plugins/play/download.py
+++ ZelzalMusic/plugins/play/download.py
@@ -1,10 +1,6 @@
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ ʑᴇʟᴢᴀʟ_ᴍᴜsɪᴄ ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/K55DD   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/K55DD ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-
 import os
 import requests
-
+import config
 import aiohttp
 import aiofiles
 
@@ -12,7 +8,7 @@ import yt_dlp
 from yt_dlp import YoutubeDL
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
-from pyrogram.types import Message, InputTextMessageContent
+from pyrogram.types import Message, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from youtube_search import YoutubeSearch
 
 from ZelzalMusic import app
@@ -23,21 +19,21 @@ def remove_if_exists(path):
         os.remove(path)
 
 
-@app.on_message(command(["/song", "بحث", "/music"]))
+@app.on_message(command(["song","/song", "بحث"]))
 async def song_downloader(client, message: Message):
     query = " ".join(message.command[1:])
     m = await message.reply_text("<b>⇜ جـارِ البحث عـن المقطـع الصـوتـي . . .</b>")
     ydl_ops = {
-        'format': 'bestaudio[ext=m4a]',
-        'keepvideo': True,
-        'prefer_ffmpeg': False,
-        'geo_bypass': True,
-        'outtmpl': '%(title)s.%(ext)s',
-        'quite': True,
+         format :  bestaudio[ext=m4a] ,
+         keepvideo : True,
+         prefer_ffmpeg : False,
+         geo_bypass : True,
+         outtmpl :  %(title)s.%(ext)s ,
+         quite : True,
     }
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
+        link = f"https://youtube.com{results[0][ url_suffix ]}"
         title = results[0]["title"][:40]
         thumbnail = results[0]["thumbnails"][0]
         thumb_name = f"{title}.jpg"
@@ -55,13 +51,14 @@ async def song_downloader(client, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"𖡃 ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ʙʏ @{app.username} "
+        rep = f"𖡃 @{app.username} "
         host = str(info_dict["uploader"])
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
             secmul *= 60
-        await m.edit("<b>⇜ جـارِ الرفـع ▬▬ . . .</b>")
+        await m.edit("<b>⇜ جـارِ التحميل ▬▬ . . .</b>")
+        
         await message.reply_audio(
             audio=audio_file,
             caption=rep,
@@ -69,6 +66,16 @@ async def song_downloader(client, message: Message):
             performer=host,
             thumb=thumb_name,
             duration=dur,
+            reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text=config.CHANNEL_NAME, url=config.CHANNEL_LINK),
+                ],
+
+            ]
+
+        ),
         )
         await m.delete()
 
@@ -81,3 +88,57 @@ async def song_downloader(client, message: Message):
         remove_if_exists(thumb_name)
     except Exception as e:
         print(e)
+
+
+@app.on_message(command(["فيديو", "video","تحميل فيديو"]))
+async def video_search(client, message):
+    ydl_opts = {
+        "format": "best",
+        "keepvideo": True,
+        "prefer_ffmpeg": False,
+        "geo_bypass": True,
+        "outtmpl": "%(title)s.%(ext)s",
+        "quite": True,
+    }
+    query = " ".join(message.command[1:])
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0][ url_suffix ]}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        # إزالة الأحرف غير الصحيحة من اسم الملف
+        title = re.sub(r [\\/*?:"<>|] ,   , title)
+        thumb_name = f"thumb{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        with open(thumb_name, "wb") as file:
+            file.write(thumb.content)
+        results[0]["duration"]
+        results[0]["url_suffix"]
+        results[0]["views"]
+        message.from_user.mention
+    except Exception as e:
+        print(e)
+    try:
+        msg = await message.reply("- يتم البحث الان .")
+        with yt_dlp.YoutubeDL(ydl_opts) as ytdl:
+            ytdl_data = ytdl.extract_info(link, download=True)
+            file_name = ytdl.prepare_filename(ytdl_data)
+    except Exception as e:
+        return await msg.edit(f"🚫 <b>error:</b> {e}")
+    thumb_path = f"thumb{title}.jpg"
+    if not os.path.exists(thumb_path):
+        return await msg.edit(f"🚫 <b>error:</b> Thumb file not found!")
+    
+    await msg.edit("- تم الرفع انتضر قليلاً .")
+    await message.reply_video(
+        file_name,
+        duration=int(ytdl_data["duration"]),
+        thumb=thumb_path,
+        caption=ytdl_data["title"],
+    )
+    try:
+        os.remove(file_name)
+        os.remove(thumb_path)
+        await msg.delete()
+    except Exception as ex:
+        print(f"- فشل : {ex}")
