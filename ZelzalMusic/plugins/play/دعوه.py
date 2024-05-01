78324cbd62468184543e53ffcc962628612ebee1
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-02 02:22:29 +0300

    Update دعوه.py

--- ZelzalMusic/plugins/play/دعوه.py
+++ ZelzalMusic/plugins/play/دعوه.py
@@ -2,10 +2,10 @@
 from config import OWNER_ID
 import asyncio
 from pyrogram import Client, filters
-from ZeMusic.utils.database import get_assistant
+from ZelzalMusic.utils.database import get_assistant
 from pyrogram.types import Message
-from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from ZeMusic.core.call import Mody
+from ZelzalMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from ZelzalMusic.core.call import Mody
 
 
 @app.on_message(filters.video_chat_started)
